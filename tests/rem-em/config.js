baf52e66e317c0f1531db7c56af9457222f312a4
noyobo <noyobo@gmail.com>
2021-07-28 16:04:21 +0800

    test: rem to em

---
+++ tests/rem-em/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  divisor: (1 / 50) * 14, // 表示 14px = 1em
+  multiple: 1,
+  decimalPlaces: 2,
+  comment: 'no',
+  targetUnits: 'em',
+  sourceUnits: 'rem',
+  declMembers: ['font-size'],
+};
